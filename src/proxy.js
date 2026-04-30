fcc72de64d3f35d6398ba7af241607fe8956fe48
amitbaraik420-hub <amitbaraik420@gmail.com>
2026-04-30 23:42:35 +0600

    prpp commit

--- src/proxy.js
+++ src/proxy.js
@@ -1,19 +1,41 @@
+// import { NextResponse } from 'next/server'
+// import Profile from './app/(profile)/profile/page'
+// import { headers } from 'next/headers';
+// import { auth } from './app/lib/auth';
+
+ 
+// // This function can be marked `async` if using `await` inside
+// export async function proxy(request) {
+//        const session = await auth.api.getSession({
+//         headers: headers(),
+//        })   
+//      if(!session){
+//         return NextResponse.redirect(new URL('/signin', request.url))
+//      }   
+// }
+ 
+// export const config = {
+//   matcher:["/Profile","/courses/:path*"] ,
+// }
 import { NextResponse } from 'next/server'
-import Profile from './app/(profile)/profile/page'
 import { headers } from 'next/headers';
 import { auth } from './app/lib/auth';
 
- 
-// This function can be marked `async` if using `await` inside
 export async function proxy(request) {
-       const session = await auth.api.getSession({
+    const session = await auth.api.getSession({
         headers: headers(),
-       })   
-     if(!session){
+    })
+
+   
+    if(!session){
         return NextResponse.redirect(new URL('/signin', request.url))
-     }   
+    }
+
+  
+    return NextResponse.next(); 
 }
- 
+
 export const config = {
-  matcher:["/Profile","/courses/:path*"] ,
+   
+    matcher: ["/profile", "/courses/:path*"] 
 }
